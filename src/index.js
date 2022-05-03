6860fc69aef5272653a5aa7342f85cbfc72310a8
Tabaja Rawan <rawantabaja12@gmail.com>
2022-05-03 10:00:31 +0200

    ok: route '/' en GET qui retourne le status code (200 ou 500)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ const http = require("http");
 const prenom = "Rawan";
 
 const server = http.createServer((req, res) => {
-	console.log("HELLO WORLD " + prenom);
+	try{
+		// console.log(a); //test error 500
+		if(req.url === "/" && req.method === "GET"){
+			res.writeHead(200, {'content-type':'text/html'});
+   		        res.write('<p>200 OK</p><h1>HELLO WORLD ' + prenom + '</h1>');
+		}
+	} catch(e){
+		res.writeHead(500, {'content-type':'text/html'});
+		res.write('<h1>500 Internal Server Error</h1>');
+	}
 	res.end();
 })
 
-server.listen(5000)
+server.listen(5000);
