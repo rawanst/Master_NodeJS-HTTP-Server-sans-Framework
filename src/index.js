d2e7a4b6e59c803ca0a58624496b329e7e6e9487
Tabaja Rawan <rawantabaja12@gmail.com>
2022-05-03 10:09:01 +0200

    ok: ajout du status code 404 pour la route '/' avec une methode autre que GET

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ const prenom = "Rawan";
 const server = http.createServer((req, res) => {
 	try{
 		// console.log(a); //test error 500
+		// req.method = "POST"; //test error 404
 		if(req.url === "/" && req.method === "GET"){
 			res.writeHead(200, {'content-type':'text/html'});
    		        res.write('<p>200 OK</p><h1>HELLO WORLD ' + prenom + '</h1>');
+		}
+		else if (req.method !== "GET"){
+			res.writeHead(404, {'content-type':'text/html'});
+			res.write('<h1>404 Méthode non authorisée</h1>');
 		}
 	} catch(e){
 		res.writeHead(500, {'content-type':'text/html'});
