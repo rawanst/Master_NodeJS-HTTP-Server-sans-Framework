6daaf32290fa958a2ea5b597f5975e504b054f39
Tabaja Rawan <rawantabaja12@gmail.com>
2022-05-03 11:05:20 +0200

    ok: route page introuvable quand status code est 404

--- src/index.js
+++ src/index.js
@@ -6,17 +6,22 @@ const server = http.createServer((req, res) => {
 	try{
 		// console.log(a); //test error 500
 		// req.method = "POST"; //test error 404
+		
 		if(req.url === "/" && req.method === "GET"){
 			res.writeHead(200, {'content-type':'text/html'});
-   		        res.write('<p>200 OK</p><h1>HELLO WORLD ' + prenom + '</h1>');
-		}
-		else if (req.method !== "GET"){
+	   		res.write('<p>200 OK</p><h1>HELLO WORLD ' + prenom + '</h1>');
+		} 
+		else if(req.url === "/" && req.method !== "GET"){
 			res.writeHead(405, {'content-type':'text/html'});
 			res.write('<h1>405 Méthode non authorisée</h1>');
 		}
+		else {
+			res.writeHead(404, {'content-type':'text/html'});
+			res.write('<h1>404 Page introuvable</h1>');
+		}
 	} catch(e){
 		res.writeHead(500, {'content-type':'text/html'});
-		res.write('<h1>500 Internal Server Error</h1>');
+		res.write('<h1>500 Erreur Interne au Server</h1>');
 	}
 	res.end();
 })
